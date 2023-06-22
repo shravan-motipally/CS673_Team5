3912875ea4485b2fd6aa8967108dfb9c4dd9dab8
Shravan Motipally <numtheory@ymail.com>
2023-06-22 18:51:03 +0100

    remove key

--- api/qBotApi/qBotApi/settings.py
+++ api/qBotApi/qBotApi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@_nz43s$-x^p8))xd$_4f#)ih($@lc8g#%%#o+$$n-+o3!wmjw'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
