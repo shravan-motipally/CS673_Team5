1d58ca01619c262dc701a0a8684e2056d6959a35
Shravan Motipally <numtheory@ymail.com>
2023-06-25 04:37:59 +0100

    connect to mongo db - success

---
+++ api/java/src/main/java/com/qbot/answeringservice/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.qbot.answeringservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/login", "/all", "/questions", "/actuator/**")
+                .permitAll()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+}
