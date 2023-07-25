978558e1fc0a781c64c04b2d074a58b20872faf7
Jacob Kustra <jacobkustra@Jacobs-MacBook-Pro.local>
2023-07-24 21:47:05 -0400

    Added/changed About page text and layout, Help page accordians, and home page text

--- webapp/src/utils/StringConstants.ts
+++ webapp/src/utils/StringConstants.ts
@@ -3,10 +3,14 @@ export const apiToken = process.env.REACT_APP_BLOOM_API_TOKEN;
 export const environment = process.env.REACT_APP_ENVIRONMENT;
 export const APPLICATION_JSON = 'application/json';
 export const HELLO_MSG = "Hi there!  Welcome to QBot!  " +
-  "I'm a question answering bot that is made to help you answer questions about CS673.  " +
-  "Feel free to ask me a question below and I'll do my best to answer you!";
+  "I am designed to answer commonly asked questions about the class you are taking with Professor Elentukh.  " +
+  "Please Feel free to ask me a question below and I'll do my best to provide you with the correct answer!";
 export const SAMPLE_USER_INPUT = "So can I ask you anything?";
-export const SAMPLE_ADVICE = "Try to keep it about this class and most likely I'll be " +
-  "able to answer you question well.  If I cannot answer a question that you have about this class, " +
-  "please seek the professor or the TA for help.  As of questions outside the scope of this class, " +
-  "I am not trained to answer such questions yet.  Hope that helps!  Now, how can I help you?"
+export const SAMPLE_ADVICE = 'Please try to have your questions pertaining to the class. In my standard mode, ' +
+    'I will do my best to accurately respond to them but if I am not able to provide you with an answer, ' +
+    'please ask your professor or TA directly for help. Although we make every effort to assure accuracy of ' +
+    'responses to your questions, we assume no responsibility or liability for any errors or omissions in the ' +
+    'content of this site. The information contained in this site is provided on an "as is" basis with no guarantees ' +
+    'of completeness, accuracy, usefulness or timeliness. However, if the professor allows you to enable ' +
+    '"Generative Mode", you may ask me any question, even those not pertaining directly to class and I ' +
+    'will use AI to respond, however it may not be completely accurate. So how can I help you today?'
