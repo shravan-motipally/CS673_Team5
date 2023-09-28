8e66c41e8393cf67ebdb674b09fd1fb05a03982f
TobyMarch <{ID}+{username}@users.noreply.github.com>
2023-09-27 22:05:34 -0400

    User & Role connection

--- api/java/src/main/java/com/qbot/answeringservice/model/Course.java
+++ api/java/src/main/java/com/qbot/answeringservice/model/Course.java
@@ -1,24 +1,20 @@
 package com.qbot.answeringservice.model;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.util.List;
-import java.util.UUID;
-
 @Getter
 @AllArgsConstructor
-@Document(collection = "user")
-public class User {
-
+@Document(collection = "course")
+public class Course {
     @Id
     private final UUID id;
-    private final String photoUrl;
-    private final UUID loginId;
-    private final String firstName;
-    private final String lastName;
-    private final List<Role> roles;
+    private final String name;
+    private final List<User> instructors;
 }
--- api/java/src/main/java/com/qbot/answeringservice/repository/UserRepository.java
+++ api/java/src/main/java/com/qbot/answeringservice/repository/UserRepository.java
@@ -4,10 +4,14 @@ import com.qbot.answeringservice.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface UserRepository extends MongoRepository<User, UUID> {
 
     @Query("{loginId:'?0'}")
     User findUserByLoginId(UUID loginId);
+
+    @Query("{ roles: ObjectId('?0') }")
+    List<User> findUsersByRole(UUID roleId);
 }
