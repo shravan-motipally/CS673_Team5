81520007cfa4bbfdee827706363208f6636799fa
Shravan Motipally <numtheory@ymail.com>
2023-11-20 02:41:44 +0000

    update url for generation

--- webapp/src/utils/Urls.ts
+++ webapp/src/utils/Urls.ts
@@ -4,7 +4,15 @@ export const getAnsweringSvcUrl = () => {
     ? "https://answering-svc.onrender.com"
     : "http://localhost:8080";
 };
+
+export const getDocRetrievalSvc = () => {
+  return environment === "prod"
+    ? "https://document-retreival-svc.onrender.com"
+    : "http://localhost:7861";
+}
 export const ANSWERING_SVC_URL = getAnsweringSvcUrl();
+
+export const DOCUMENT_RETRIEVAL_SVC_URL = getDocRetrievalSvc();
 export const HUGGINGFACE_INFERENCE_API =
   "https://api-inference.huggingface.co/models";
 // ANSWERING SVC APIs
@@ -20,7 +28,7 @@ export const deleteDocumentUrl = (documentId: string) => ANSWERING_SVC_URL + "/d
 export const bulkUploadCoursesUrl = ()  =>  ANSWERING_SVC_URL + "/courses/all"
 export const loginUrl = () => ANSWERING_SVC_URL + "/login";
 export const healthUrl = () => ANSWERING_SVC_URL + "/actuator/health";
-export const getOpenAIUrl = () => "http://localhost:7861/chat";
+export const getOpenAIUrl = () => DOCUMENT_RETRIEVAL_SVC_URL + "/chat";
 // Generative models
 export const GPT2 = "gpt2";
 export const BLOOM = "bigscience/bloom";
