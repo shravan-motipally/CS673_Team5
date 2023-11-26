7d03def0c0f71dc3bc927ec20dcaa0ba18a06009
TobyMarch <{ID}+{username}@users.noreply.github.com>
2023-11-25 20:59:06 -0500

    default formatting

--- webapp/src/components/Container.tsx
+++ webapp/src/components/Container.tsx
@@ -22,17 +22,17 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 
-import {useCallback, useContext, useEffect, useMemo, useRef, useState} from 'react';
+import { useCallback, useContext, useEffect, useMemo, useRef, useState } from 'react';
 import { ScreenContext } from '../App';
 import ai from '../screens/images/botTransparentWhite.png';
 
 import Avatar from '@mui/material/Avatar';
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
-import {Drawer, Menu, MenuItem, Switch, Tooltip} from "@mui/material";
-import {darkTheme, lightTheme} from "../utils/Themes";
-import {Logout} from "@mui/icons-material";
-import {Exchange} from "../screens/Edit";
-import {isAdministrator, isEducator} from "../utils/RoleUtils";
+import { Drawer, Menu, MenuItem, Switch, Tooltip } from "@mui/material";
+import { darkTheme, lightTheme } from "../utils/Themes";
+import { Logout } from "@mui/icons-material";
+import { Exchange } from "../screens/Edit";
+import { isAdministrator, isEducator } from "../utils/RoleUtils";
 import { Role } from '../types/global.types';
 
 const DrawerHeader = styled('div')(({ theme }) => ({
@@ -68,10 +68,10 @@ const AppBar = styled(MuiAppBar, {
 
 
 interface ContainerProps {
-	children: any
+  children: any
 }
 
-const Container: React.FC<ContainerProps> = ( { children } ) => {
+const Container: React.FC<ContainerProps> = ({ children }) => {
   const { screenState, setScreenState } = useContext(ScreenContext);
   const [logoutAnchor, setLogoutAnchor] = React.useState<null | HTMLElement>(null);
   const [courseAnchor, setCourseAnchor] = React.useState<null | HTMLElement>(null);
@@ -176,7 +176,7 @@ const Container: React.FC<ContainerProps> = ( { children } ) => {
 
   return (
     <ThemeProvider theme={screenState.darkMode ? darkTheme : lightTheme} >
-      <Box sx={{ display: 'flex'}}>
+      <Box sx={{ display: 'flex' }}>
         <CssBaseline />
         <AppBar position="fixed" open={open} sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }} >
           <Toolbar>
@@ -252,15 +252,15 @@ const Container: React.FC<ContainerProps> = ( { children } ) => {
                   anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
                 >
                   <MenuItem onClick={() => {
-                    setScreenState( { ...screenState, screen: 'manage' })
+                    setScreenState({ ...screenState, screen: 'manage' })
                   }}>
                     <ListItemIcon>
-                      <StorageIcon fontSize="small" color={"primary"}/>
+                      <StorageIcon fontSize="small" color={"primary"} />
                     </ListItemIcon>
                     Manage
                   </MenuItem>
                   <MenuItem onClick={() => {
-                    setScreenState( { ...screenState, screen: 'config' })
+                    setScreenState({ ...screenState, screen: 'config' })
                   }}>
                     <ListItemIcon>
                       <SettingsIcon fontSize="small" />
@@ -284,45 +284,14 @@ const Container: React.FC<ContainerProps> = ( { children } ) => {
         }} >
           <Divider />
           <Toolbar />
-          <List sx={{ paddingBottom:0, paddingTop: 0 }}>
-            { screenState.currentClass !== null ?  [screenState.currentClassName + ' Home'].map((text, index) => (
-                <ListItem
-                  key={"ContainerKey-" + text}
-                  disablePadding sx={{ display: 'block' }}
-                  onClick={() => {
-                    setScreenState( { ...screenState, screen: 'home' });
-                  }}>
-                  <ListItemButton
-                      sx={{
-                        minHeight: 48,
-                        justifyContent: open ? 'initial' : 'center',
-                        px: 2.5,
-                      }}
-                  >
-                    <ListItemIcon
-                        sx={{
-                          minWidth: 0,
-                          mr: open ? 3 : 'auto',
-                          justifyContent: 'center',
-                        }}
-                    >
-                      <HomeIcon />
-                    </ListItemIcon>
-                    <ListItemText primary={text} color={"secondary"} sx={{m:1}} />
-                  </ListItemButton>
-                </ListItem>
-            )) : <div/>}
-          </List>
-          <List sx={{ paddingTop: 0, paddingBottom: 0 }}>
-            {['Help', 'About'].map((text, index) => (
+          <List sx={{ paddingBottom: 0, paddingTop: 0 }}>
+            {screenState.currentClass !== null ? [screenState.currentClassName + ' Home'].map((text, index) => (
               <ListItem
                 key={"ContainerKey-" + text}
                 disablePadding sx={{ display: 'block' }}
-                onClick={ () => {
-                  setScreenState( {
-                    ...screenState,
-                    screen: index % 2 === 0 ?  'help' : 'about' });
-                } }>
+                onClick={() => {
+                  setScreenState({ ...screenState, screen: 'home' });
+                }}>
                 <ListItemButton
                   sx={{
                     minHeight: 48,
@@ -333,19 +302,51 @@ const Container: React.FC<ContainerProps> = ( { children } ) => {
                   <ListItemIcon
                     sx={{
                       minWidth: 0,
-                      mr:  open ? 3 : 'auto',
+                      mr: open ? 3 : 'auto',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    <HomeIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={text} color={"secondary"} sx={{ m: 1 }} />
+                </ListItemButton>
+              </ListItem>
+            )) : <div />}
+          </List>
+          <List sx={{ paddingTop: 0, paddingBottom: 0 }}>
+            {['Help', 'About'].map((text, index) => (
+              <ListItem
+                key={"ContainerKey-" + text}
+                disablePadding sx={{ display: 'block' }}
+                onClick={() => {
+                  setScreenState({
+                    ...screenState,
+                    screen: index % 2 === 0 ? 'help' : 'about'
+                  });
+                }}>
+                <ListItemButton
+                  sx={{
+                    minHeight: 48,
+                    justifyContent: open ? 'initial' : 'center',
+                    px: 2.5,
+                  }}
+                >
+                  <ListItemIcon
+                    sx={{
+                      minWidth: 0,
+                      mr: open ? 3 : 'auto',
                       justifyContent: 'center',
                     }}
                   >
                     {index % 2 === 0 ? <HelpOutlineIcon /> : <InfoIcon />}
                   </ListItemIcon>
-                  <ListItemText primary={text} color={"secondary"} sx={{m:1}} />
+                  <ListItemText primary={text} color={"secondary"} sx={{ m: 1 }} />
                 </ListItemButton>
               </ListItem>
             ))}
           </List>
-          <List sx={{ paddingTop: 0}}>
-            <ListItem key={"ContainerKey-Settings"} disablePadding sx={{ display: 'block' }} onClick={ () => { setScreenState( { ...screenState, screen: 'config'  }); } }>
+          <List sx={{ paddingTop: 0 }}>
+            <ListItem key={"ContainerKey-Settings"} disablePadding sx={{ display: 'block' }} onClick={() => { setScreenState({ ...screenState, screen: 'config' }); }}>
               <ListItemButton
                 sx={{
                   minHeight: 48,
@@ -362,15 +363,15 @@ const Container: React.FC<ContainerProps> = ( { children } ) => {
                 >
                   <SettingsIcon />
                 </ListItemIcon>
-                <ListItemText primary={"Settings"} color={"secondary"} sx={{m:1}}/>
+                <ListItemText primary={"Settings"} color={"secondary"} sx={{ m: 1 }} />
               </ListItemButton>
             </ListItem>
           </List>
-          { screenState.isAuthed && isEducatorCallback(screenState.roles) ?
+          {screenState.isAuthed && isEducatorCallback(screenState.roles) ?
             <div>
               <Divider />
               <List>
-                <ListItem key={"ContainerKey-Manage"} disablePadding sx={{ display: 'block' }} onClick={ () => { setScreenState( { ...screenState, screen: 'manage'  }); } }>
+                <ListItem key={"ContainerKey-Manage"} disablePadding sx={{ display: 'block' }} onClick={() => { setScreenState({ ...screenState, screen: 'manage' }); }}>
                   <ListItemButton
                     sx={{
                       minHeight: 48,
@@ -387,18 +388,18 @@ const Container: React.FC<ContainerProps> = ( { children } ) => {
                     >
                       <StorageIcon />
                     </ListItemIcon>
-                    <ListItemText primary={"Manage"} color={"secondary"} sx={{m:1}}/>
+                    <ListItemText primary={"Manage"} color={"secondary"} sx={{ m: 1 }} />
                   </ListItemButton>
                 </ListItem>
               </List>
             </div> :
-            <div/>
+            <div />
           }
-          { screenState.isAuthed && isAdministratorCallback(screenState.roles) ?
+          {screenState.isAuthed && isAdministratorCallback(screenState.roles) ?
             <div>
               <Divider />
               <List>
-                <ListItem key={"ContainerKey-Admin"} disablePadding sx={{ display: 'block' }} onClick={ () => { setScreenState( { ...screenState, screen: 'admin'  }); } }>
+                <ListItem key={"ContainerKey-Admin"} disablePadding sx={{ display: 'block' }} onClick={() => { setScreenState({ ...screenState, screen: 'admin' }); }}>
                   <ListItemButton
                     sx={{
                       minHeight: 48,
@@ -415,18 +416,18 @@ const Container: React.FC<ContainerProps> = ( { children } ) => {
                     >
                       <StorageIcon />
                     </ListItemIcon>
-                    <ListItemText primary={"Administration"} color={"secondary"} sx={{m:1}}/>
+                    <ListItemText primary={"Administration"} color={"secondary"} sx={{ m: 1 }} />
                   </ListItemButton>
                 </ListItem>
               </List>
             </div> :
-            <div/>
+            <div />
           }
         </Drawer>
 
 
 
-        <Box component="main" sx={{ flexGrow: 1, p: 3}}>
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
           <DrawerHeader />
           {children}
         </Box>
