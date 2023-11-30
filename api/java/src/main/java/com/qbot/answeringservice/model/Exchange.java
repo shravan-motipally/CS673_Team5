c30d2aa1210b5f6255f38cc95f61e78c76c01648
TobyMarch <{ID}+{username}@users.noreply.github.com>
2023-11-29 19:37:18 -0500

    Exchange model class cleanup

--- api/java/src/main/java/com/qbot/answeringservice/model/Exchange.java
+++ api/java/src/main/java/com/qbot/answeringservice/model/Exchange.java
@@ -1,7 +1,6 @@
 package com.qbot.answeringservice.model;
 
 import org.mongojack.Id;
-import org.mongojack.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Getter;
@@ -11,15 +10,14 @@ import lombok.Setter;
 @Document(collection = "exchange")
 public class Exchange {
     @Id
-    @ObjectId
     private String id;
     @Setter
     private String courseId;
     private String question;
     private String answer;
 
-    public Exchange(String exchangeId, String courseId, String question, String answer) {
-        this.id = exchangeId;
+    public Exchange(String id, String courseId, String question, String answer) {
+        this.id = id;
         this.courseId = courseId;
         this.question = question;
         this.answer = answer;
